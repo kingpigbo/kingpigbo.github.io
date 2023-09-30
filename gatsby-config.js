b55b0cc6e578d505dea88e1498ae4475b4b575c5
mong <mong@channel.io>
2023-09-30 22:53:41 +0900

    1차 검수

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
         },
         contentDirectory: "./content",
         blogSettings: {
+          entityName: "Article",
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
